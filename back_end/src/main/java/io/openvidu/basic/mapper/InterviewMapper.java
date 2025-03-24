74f34b926b689c71de889f723d38d87a543646a8
YKX <984543219@qq.com>
2025-03-24 17:45:18 +0800

    init

--- back_end/src/main/java/io/openvidu/basic/mapper/InterviewMapper.java
+++
@@ -1,7 +0,0 @@
-package io.openvidu.basic.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import io.openvidu.basic.entity.Interview;
-
-public interface InterviewMapper extends BaseMapper<Interview> {
-}
